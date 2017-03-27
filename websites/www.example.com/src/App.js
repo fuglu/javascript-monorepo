26d72258d762d7e6b595edb8c91286e1b3d148e8
Benjamin Kluck <ben@fug.lu>
2017-03-28 00:52:39 +0200

    Add header and footer to www.example.com

--- websites/www.example.com/src/App.js
+++ websites/www.example.com/src/App.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import Header from '@org/react-header';
+import Footer from '@org/react-footer';
 import logo from './logo.svg';
 import './App.css';
 
@@ -6,13 +8,14 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
+        <Header>
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
-        </div>
+        </Header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <Footer>Footer deluxe!</Footer>
       </div>
     );
   }
