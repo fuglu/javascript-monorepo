4e6b98e61a26afc1c12628c9da434318361f0367
Benjamin Kluck <ben@fug.lu>
2017-03-27 22:16:04 +0200

    Remove react-example-component

--- components/react-example-component/src/index.stories.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { storiesOf } from '@kadira/storybook';
-import Component from './';
-
-storiesOf('Component', module)
-  .add('default', () => (
-    <Component />
-  ));
