845e5304c2fd38b36af2f2bcffab7c9fc09da00c
Benjamin Kluck <ben@fug.lu>
2017-03-28 00:39:30 +0200

    Add react-link to react-footer storybooks

--- components/react-footer/src/index.stories.jsx
+++ components/react-footer/src/index.stories.jsx
@@ -1,15 +1,16 @@
 import React from 'react';
 import { storiesOf } from '@kadira/storybook';
+import Link from '@org/react-link';
 import Footer from './';
 
 storiesOf('Footer', module)
   .add('containing some links', () => (
     <Footer>
       <div>
-        <a href={'https://github.com/fuglu'}>github</a>
+        <Link href={'https://github.com/fuglu'}>github</Link>
       </div>
       <div>
-        <a href={'https://twitter.com/fuglu'}>twitter</a>
+        <Link href={'https://twitter.com/fuglu'}>twitter</Link>
       </div>
     </Footer>
   ));
