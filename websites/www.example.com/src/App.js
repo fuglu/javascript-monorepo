9f5fad1e46dfd2f7806574147f200ba1b24e4b8f
Benjamin Kluck <ben@fug.lu>
2017-03-29 01:11:04 +0200

    Use react-headline in www.example.com

--- websites/www.example.com/src/App.js
+++ websites/www.example.com/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Header from '@org/react-header';
+import Headline from '@org/react-headline';
 import Footer from '@org/react-footer';
 import logo from './logo.svg';
 import './App.css';
@@ -10,7 +11,7 @@ class App extends Component {
       <div className="App">
         <Header>
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <Headline>Welcome to React</Headline>
         </Header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
